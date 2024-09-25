c24e306a2dc38387b317d6d4d7dd8139dc5939b4
Tristan <No>
2024-09-25 15:00:23 -0300

    Have to finish madlib Story

--- src/App.java
+++ src/App.java
@@ -36,7 +36,34 @@ public class App {
 
     }
 
-    public void doMadlib() {}
+    public void doMadlib() {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Verb 1:");
+        String verb = input.nextLine(); 
+        System.out.println("noun 1");
+        String noun = input.nextLine();
+        System.out.println("adverb 1");
+        String adverb = input.nextLine();
+        System.out.println("pronoun"); 
+        String pronoun = input.nextLine();
+        System.out.println(" Verb 2");
+        String verb2 = input.nextLine();
+        System.out.println("noun 2");
+        String noun2 = input.nextLine();
+        System.out.println("adverb 2 ");
+        String adverb2 = input.nextLine();
+        System.out.println("pronoun 2");
+        String pronoun2 = input.nextLine();
+        System.out.println("person 1");
+        String person = input.nextLine();
+        System.out.println("person 2");
+        String secondPerson = input.nextLine();
+
+
+
+    }
+
     
     /* DO NOT TOUCH OR EDIT THE METHOD BELOW! ADD NO CODE BELOW THIS LINE */
     public static void main(String[] args) throws Exception {
